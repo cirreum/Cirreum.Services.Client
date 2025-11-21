1cb859bcf08ff9a62f4ea6e1afc35641e9c27034
Glen Banta <glen.banta@outlook.com>
2025-11-21 05:17:18 -0700

    updated the nuget packages fix the not implemented file system file.

--- src/Cirreum.Services.Client/FileSystem/NotImplementedFileSystem.cs
+++ src/Cirreum.Services.Client/FileSystem/NotImplementedFileSystem.cs
@@ -2,109 +2,101 @@
 
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 
 sealed class NotImplementedFileSystem : IFileSystem {
 
-	public void CopyDirectory(string sourceDirName, string destDirName, bool copySubDirs, bool overwrite = false) {
+	void IFileSystem.CopyDirectory(string sourceDirName, string destDirName, bool copySubDirs, bool overwrite) {
 		throw new NotImplementedException();
 	}
 
-	public void CopyFile(string sourceFileName, string destFileName, bool overwrite = false) {
+	void IFileSystem.CopyFile(string sourceFileName, string destFileName, bool overwrite) {
 		throw new NotImplementedException();
 	}
 
-	public void DeleteChildDirectories(string rootPath) {
+	void IFileSystem.DeleteChildDirectories(string rootPath) {
 		throw new NotImplementedException();
 	}
 
-	public void DeleteDirectory(string path, bool recursive) {
+	void IFileSystem.DeleteDirectory(string path, bool recursive) {
 		throw new NotImplementedException();
 	}
 
-	public void DeleteFile(string path) {
+	void IFileSystem.DeleteFile(string path) {
 		throw new NotImplementedException();
 	}
 
-	public void DeleteFileWithRetry(string path) {
+	void IFileSystem.DeleteFileWithRetry(string path) {
 		throw new NotImplementedException();
 	}
 
-	public bool DirectoryExists(string path) {
+	bool IFileSystem.DirectoryExists(string path) {
 		throw new NotImplementedException();
 	}
 
-	public bool EnsureDirectory(string path) {
+	bool IFileSystem.EnsureDirectory(string path) {
 		throw new NotImplementedException();
 	}
 
-	public void ExtractSevenZFile(string source, string destination, int secondsToWait = 30) {
+	void IFileSystem.ExtractZipFile(string source, string destination, bool overwriteFiles) {
 		throw new NotImplementedException();
 	}
 
-	public void ExtractZipFile(string source, string destination, bool overwriteFiles) {
+	bool IFileSystem.FileExists(string path) {
 		throw new NotImplementedException();
 	}
 
-	public bool FileExists(string path) {
+	string[] IFileSystem.GetFiles(string path, string searchPattern, bool includeChildDirectories) {
 		throw new NotImplementedException();
 	}
 
-	public string[] GetFiles(string path, string searchPattern, bool includeChildDirectories) {
+	void IFileSystem.MoveDirectory(string sourceDirName, string destDirName) {
 		throw new NotImplementedException();
 	}
 
-	public void MoveDirectory(string sourceDirName, string destDirName) {
+	void IFileSystem.MoveFile(string sourceFileName, string destFileName, bool overwrite) {
 		throw new NotImplementedException();
 	}
 
-	public void MoveFile(string sourceFileName, string destFileName, bool overwrite = false) {
+	IEnumerable<string> IFileSystem.QueryDirectories(string[] paths, bool includeChildDirectories, string searchPattern, Func<string, bool>? predicate, int take) {
 		throw new NotImplementedException();
 	}
 
-	public IEnumerable<string> QueryDirectories(string[] paths, bool includeChildDirectories, string searchPattern, int take = 0, Func<string, bool>? predicate = null) {
+	IEnumerable<string> IFileSystem.QueryDirectories(string path, bool includeChildDirectories, string searchPattern, Func<string, bool>? predicate, int take) {
 		throw new NotImplementedException();
 	}
 
-	public IEnumerable<string> QueryDirectories(string path, bool includeChildDirectories, string searchPattern, int take = 0, Func<string, bool>? predicate = null) {
+	IEnumerable<string> IFileSystem.QueryDirectories(string path, bool includeChildDirectories, IEnumerable<string> searchPatterns, Func<string, bool>? predicate, int take) {
 		throw new NotImplementedException();
 	}
 
-	public IEnumerable<string> QueryDirectories(string path, bool includeChildDirectories, IEnumerable<string> searchPatterns, int take = 0, Func<string, bool>? predicate = null) {
+	IEnumerable<string> IFileSystem.QueryFiles(string[] paths, bool includeChildDirectories, string searchPattern, Func<string, bool>? predicate, int take) {
 		throw new NotImplementedException();
 	}
 
-	public long QueryFileCount(string directory, bool includeChildDirectories, string searchPattern, Func<string, bool>? predicate = null, FileAttributes attributesToSkip = FileAttributes.Hidden | FileAttributes.System) {
+	IEnumerable<string> IFileSystem.QueryFiles(string path, bool includeChildDirectories, string searchPattern, Func<string, bool>? predicate, int take) {
 		throw new NotImplementedException();
 	}
 
-	public IEnumerable<string> QueryFiles(string[] paths, bool includeChildDirectories, string searchPattern, int take = 0, Func<string, bool>? predicate = null) {
+	IEnumerable<string> IFileSystem.QueryFiles(string path, bool includeChildDirectories, IEnumerable<string> searchPatterns, Func<string, bool>? predicate, int take) {
 		throw new NotImplementedException();
 	}
 
-	public IEnumerable<string> QueryFiles(string path, bool includeChildDirectories, string searchPattern, int take = 0, Func<string, bool>? predicate = null) {
+	string IFileSystem.ReadAllText(string path) {
 		throw new NotImplementedException();
 	}
 
-	public IEnumerable<string> QueryFiles(string path, bool includeChildDirectories, IEnumerable<string> searchPatterns, int take = 0, Func<string, bool>? predicate = null) {
+	Task<string> IFileSystem.ReadAllTextAsync(string path, CancellationToken cancellationToken) {
 		throw new NotImplementedException();
 	}
 
-	public void WriteAllText(string path, string contents) {
+	void IFileSystem.WriteAllText(string path, string contents) {
 		throw new NotImplementedException();
 	}
 
-	public Task WriteAllTextAsync(string path, string contents, CancellationToken cancellationToken = default) {
+	Task IFileSystem.WriteAllTextAsync(string path, string contents, CancellationToken cancellationToken) {
 		throw new NotImplementedException();
 	}
 
-	public string ReadAllText(string path) {
-		throw new NotImplementedException();
-	}
-
-	public Task<string> ReadAllTextAsync(string path, CancellationToken cancellationToken = default) {
-		throw new NotImplementedException();
-	}
 }
